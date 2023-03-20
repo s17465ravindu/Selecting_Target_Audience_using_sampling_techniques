5aac1e7a816fe2181eec67aa450cacff08f3cc17
s17465ravindu <117331506+s17465ravindu@users.noreply.github.com>
2023-03-20 11:16:00 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -8,7 +8,7 @@ Original file is located at
 """
 import pandas as pd
 import numpy as np
-import scikit-learn
+import sklearn
 #from pydrive.auth import GoogleAuth
 #from pydrive.drive import GoogleDrive
 #from google.colab import auth
