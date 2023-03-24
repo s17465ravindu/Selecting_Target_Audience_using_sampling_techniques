e827c42cc4c160d11d9c34c040e5d900654a0a85
s17465ravindu <117331506+s17465ravindu@users.noreply.github.com>
2023-03-24 20:28:01 +0530

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -42,11 +42,11 @@ if file is not None:
         
         for size in sample_sizes:
             split = StratifiedShuffleSplit(n_splits=1, test_size=size)
-            for train_index, test_index in split.split(data2, data2['Customer_Strata']):
-                str_sample = data2.iloc[test_index].sort_values(by='cust_id').head(size)
-            str_mean = str_sample['total_discount_received'].mean()
-            str_se = np.std(str_sample['total_discount_received'], ddof=1) / np.sqrt(size)
-            str_absolute_error = abs(population_mean - str_mean)
+            for x, y in split.split(data2, data2['Customer_Strata']):
+                str_sample = data2.iloc[y].sort_values(by='cust_id')
+                str_mean = str_sample['total_discount_received'].mean()
+                str_se = np.std(str_sample['total_discount_received'], ddof=1) / np.sqrt(size)
+                str_absolute_error = abs(population_mean - str_mean)
             samples.append(['Stratified Sampling', size, str_absolute_error, str_se])
         return samples 
 
