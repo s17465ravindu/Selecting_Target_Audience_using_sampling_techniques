58108e6608332b2d0f51dbf14a4aeac7d6fff0d3
s17465ravindu <117331506+s17465ravindu@users.noreply.github.com>
2023-03-27 12:18:19 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -2,27 +2,19 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle
-import time
-from matplotlib import pyplot as plt
-from  matplotlib.ticker import FuncFormatter
-import seaborn as sns
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-from google.colab import auth
-from oauth2client.client import GoogleCredentials
-import plotly.express as px
-import seaborn as sns
-import matplotlib.pyplot as plt
-import json
-import requests
-auth.authenticate_user()
-gauth = GoogleAuth()
-gauth.credentials = GoogleCredentials.get_application_default()
-drive = GoogleDrive(gauth)
 
-#link = 'https://drive.google.com/open?id=1G0KdR1AxGkEAic-1VCaNi7NUK2YdkupQ'
-#fluff, id = link.split('=')
-id = '100934670427194582945'
-downloaded = drive.CreateFile({'id':id}) 
-downloaded.GetContentFile('sales_df_clean.csv')  
-sales_df = pd.read_csv('sales_df_clean.csv')
+selected_day = st.selectbox(
+    "Select 👇", ('Age distribution of completed orders','category'))
+
+with st.expander("About the #30DaysOfStreamlit"):
+    st.markdown(
+        """
+    The **#30DaysOfStreamlit** is a coding challenge designed to help you get started in building Streamlit apps.
+    
+    Particularly, you'll be able to:
+    - Set up a coding environment for building Streamlit apps
+    - Build your first Streamlit app
+    - Learn about all the awesome input/output widgets to use for your Streamlit app
+    """
+    )
+
