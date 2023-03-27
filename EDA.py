68ba72f5878bbf0110e6ada760a941b3b11fe30a
s17465ravindu <117331506+s17465ravindu@users.noreply.github.com>
2023-03-27 11:08:04 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -6,5 +6,23 @@ import time
 from matplotlib import pyplot as plt
 from  matplotlib.ticker import FuncFormatter
 import seaborn as sns
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from google.colab import auth
+from oauth2client.client import GoogleCredentials
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+import json
+import requests
+auth.authenticate_user()
+gauth = GoogleAuth()
+gauth.credentials = GoogleCredentials.get_application_default()
+drive = GoogleDrive(gauth)
 
-df_database = pd.read_excel("./data/data_BuLi_13_20_cleaned.csv")
+#link = 'https://drive.google.com/open?id=1G0KdR1AxGkEAic-1VCaNi7NUK2YdkupQ'
+#fluff, id = link.split('=')
+id = '100934670427194582945'
+downloaded = drive.CreateFile({'id':id}) 
+downloaded.GetContentFile('sales_df_clean.csv')  
+sales_df = pd.read_csv('sales_df_clean.csv')
