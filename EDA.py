f0eec791148f691b4fa5de5c769c9dd413a6159f
s17465ravindu <117331506+s17465ravindu@users.noreply.github.com>
2023-03-27 12:20:30 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -3,18 +3,19 @@ import numpy as np
 import pandas as pd
 import pickle
 
-selected_day = st.selectbox(
-    "Select 👇", ('Age distribution of completed orders','category'))
+def test:
+    selected_day = st.selectbox(
+        "Select 👇", ('Age distribution of completed orders','category'))
 
-with st.expander("About the #30DaysOfStreamlit"):
-    st.markdown(
+    with st.expander("About the #30DaysOfStreamlit"):
+        st.markdown(
+            """
+        The **#30DaysOfStreamlit** is a coding challenge designed to help you get started in building Streamlit apps.
+
+        Particularly, you'll be able to:
+        - Set up a coding environment for building Streamlit apps
+        - Build your first Streamlit app
+        - Learn about all the awesome input/output widgets to use for your Streamlit app
         """
-    The **#30DaysOfStreamlit** is a coding challenge designed to help you get started in building Streamlit apps.
-    
-    Particularly, you'll be able to:
-    - Set up a coding environment for building Streamlit apps
-    - Build your first Streamlit app
-    - Learn about all the awesome input/output widgets to use for your Streamlit app
-    """
-    )
+        )
 
